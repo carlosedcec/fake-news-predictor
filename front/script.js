28d1b0b67f66533d3b36b972d2d8ce960a917631
Carlos Eduardo <carloseduardo@ceccon.com.br>
2026-03-27 21:44:17 -0300

    get de notícias no front

--- front/script.js
+++ front/script.js
@@ -1,3 +1,47 @@
+// ------------------------------
+// GET NEWS
+//-------------------------------
+function getNews() {
+
+    let url = 'http://127.0.0.1:5000/news';
+
+    fetch(url, {
+        method: 'get'
+    })
+        .then(async (response) => {
+            const data = await response.json();
+            if (!response.ok)
+                throw new Error(data.message || "Request failed");
+            return data;
+        })
+        .then((data) => {
+            const news = data.news;
+            if (news && news.length > 0 && news[0].id) {
+                const tbody = document.querySelector("table tbody");
+                news.forEach((news) => {
+                    const tr = document.createElement("tr");
+                    tr.innerHTML += `<td><div class="cell-clamp">${news.title}</div></td>`
+                    tr.innerHTML += `<td><div class="cell-clamp">${news.text}</div></td>`
+                    const label = Number(news.label) ? "Real" : "Fake";
+                    tr.innerHTML += `<td class="label label-${label.toLowerCase()}"><div class="cell-clamp">${label}</div></td>`
+                    tr.innerHTML += `<td class="delete"><div class="cell-clamp"><button><img src="img/trash-icon.png"></button></div></td>`
+                    tbody.appendChild(tr);
+                });
+            }
+        })
+        .catch((error) => {
+            alert(error);
+            throw error;
+        });
+
+}
+
+getNews();
+
+// ------------------------------
+// FORM
+//-------------------------------
+
 function onFormSubmit(event) {
 
     event.preventDefault();
@@ -92,8 +136,9 @@ formElements.forEach((item) => {
     item.addEventListener("change", (event) => item.classList.remove("invalid"));
 })
 
-function showNews(event) {
-    document.querySelector("body").classList.toggle("full");
-}
+// ------------------------------
+// SHOW/CLOSE NEWS
+//-------------------------------
 
-document.getElementById("showNews").addEventListener("click", showNews);
+document.getElementById("showNews").addEventListener("click", () => document.querySelector("body").classList.add("full"));
+document.getElementById("closeNews").addEventListener("click", () => document.querySelector("body").classList.remove("full"));
